1a03a73eec9e1a3a3d5d95cc3c7782b88c8bf453
esther-n-m <esthermutembei50@gmail.com>
2025-08-09 17:01:41 +0300

    error correction

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
 from . import views
 from .views import list_books, LibraryDetailView ,register_view, login_view, logout_view
+from django.contrib.auth.views import LoginView, LogoutView
+
+
 
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),  # FBV URL
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # CBV URL
     
-     # Auth URLs
+    # Authentication URLs using built-in class-based views
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    # Registration is custom, so keep your function view
     path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
 ]
