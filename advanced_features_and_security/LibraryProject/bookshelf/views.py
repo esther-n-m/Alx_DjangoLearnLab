cbcaba0331e717529ae20669ffeb9e0f7540e2f7
esther-n-m <esthermutembei50@gmail.com>
2025-08-11 14:27:07 +0300

    Implement and manage permissions and groups to control access to various parts of your Django application, enhancing security and functionality.

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+
+@permission_required('bookshelf.can_view')
+def view_books(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/view_books.html', {'books': books})
+
+@permission_required('bookshelf.can_create')
+def create_book(request):
+    # Logic to create book
+    return render(request, 'bookshelf/create_book.html')
+
+@permission_required('bookshelf.can_edit')
+def edit_book(request, book_id):
+    # Logic to edit book
+    return render(request, 'bookshelf/edit_book.html')
+
+@permission_required('bookshelf.can_delete')
+def delete_book(request, book_id):
+    # Logic to delete book
+    return render(request, 'bookshelf/delete_book.html')
