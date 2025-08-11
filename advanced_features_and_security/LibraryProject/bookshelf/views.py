c990b858dae5e9b1258120cc2d0a17441ecbf2db
esther-n-m <esthermutembei50@gmail.com>
2025-08-11 14:30:45 +0300

    Rename view to book_list and add raise_exception=True in permission decorator

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,10 +4,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 
-@permission_required('bookshelf.can_view')
-def view_books(request):
-    books = Book.objects.all()
-    return render(request, 'bookshelf/view_books.html', {'books': books})
+
 
 @permission_required('bookshelf.can_create')
 def create_book(request):
@@ -23,3 +20,8 @@ def edit_book(request, book_id):
 def delete_book(request, book_id):
     # Logic to delete book
     return render(request, 'bookshelf/delete_book.html')
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
