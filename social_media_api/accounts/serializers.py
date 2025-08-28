8973bcca8c759c2618e2f70f1bf7b4a5213f1599
esther-n-m <esthermutembei50@gmail.com>
2025-08-28 12:59:48 +0300

    Use get_user_model().objects.create_user in RegisterSerializer for user creation

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,8 @@
+from django.contrib.auth import authenticate, get_user_model
 from rest_framework import serializers
-from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
-from .models import User
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,9 +17,10 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ["username", "email", "password"]
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+       
+        user = get_user_model().objects.create_user(
             username=validated_data["username"],
-            email=validated_data["email"],
+            email=validated_data.get("email"),
             password=validated_data["password"],
         )
         Token.objects.create(user=user)
