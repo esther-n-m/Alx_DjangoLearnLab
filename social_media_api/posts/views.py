cc7b107251e644360aeadd184479e4f52a38baba
esther-n-m <esthermutembei50@gmail.com>
2025-08-28 13:52:43 +0300

    error correction: posts/views file including  permissions.IsAuthenticated

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,17 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import status, permissions
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Post
 from .serializers import PostSerializer
 
+
 class FeedView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
-        # Get posts from users the current user follows
-        following_users = request.user.following.all()
-        posts = Post.objects.filter(author__in=following_users).order_by("-created_at")
+        # Get all posts from users the current user follows
+        followed_users = request.user.following.all()
+        posts = Post.objects.filter(author__in=followed_users).order_by("-created_at")
         serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
