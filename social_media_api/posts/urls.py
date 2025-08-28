0bde6c46d254f7c89d7be4097b71d50e1463576c
esther-n-m <esthermutembei50@gmail.com>
2025-08-28 15:09:36 +0300

    Add like endpoint to posts/urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import FeedView , LikePostView, UnlikePostView
+from .views import FeedView , LikePostView, UnlikePostView , PostListCreateView, PostDetailView
 
 urlpatterns = [
     path("feed/", FeedView.as_view(), name="feed"),
-    path("like/<int:pk>/", LikePostView.as_view(), name="like-post"),
+    
     path("<int:pk>/unlike/", UnlikePostView.as_view(), name="unlike-post"),
+    path("", PostListCreateView.as_view(), name="post-list-create"),
+    path("<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("<int:pk>/like/", LikePostView.as_view(), name="post-like"), 
+    
 ]
