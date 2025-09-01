c7e6fe847582663bea48184ae2df514b0a344271
esther-n-m <esthermutembei50@gmail.com>
2025-09-01 23:28:09 +0300

    Add explicit update and delete endpoints for Book model in api/urls.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 
 #  Anyone can read, only authenticated users can write
 # List all books
