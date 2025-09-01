80ebd0769dedd91366b11afd0efa125937609801
esther-n-m <esthermutembei50@gmail.com>
2025-09-02 00:23:39 +0300

    adding port to my database

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,6 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'esie',         # required
+        'PASSWORD': '1234', # required
+        'HOST': 'localhost',            # or your db host
+        'PORT': '3307',  
     }
 }
 
