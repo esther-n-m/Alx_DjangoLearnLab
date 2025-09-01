5528d3ceb049fa0f17ab65ababcfd40a35d817fa
esther-n-m <esthermutembei50@gmail.com>
2025-09-02 02:11:22 +0300

    error correction

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -9,13 +9,14 @@ def get_absolute_url(self):
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    tags = models.ManyToManyField('Tag', related_name='posts', blank=True)  
+    tags = TaggableManager() 
 
     def __str__(self):
         return self.title
